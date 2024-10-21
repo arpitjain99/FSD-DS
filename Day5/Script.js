35830557e57d96cd8913b5e2975b941e27470bf1
arpitjain99 <aj2305988@gmail.com>
2024-10-21 09:15:36 +0530

    non filter reduce

---
+++ Day5/Script.js
@@ -0,0 +1,42 @@
+function display() {
+    let p1=document.querySelector("p");
+    p1.innerHTML="Result";
+    p1.style.backgroundcolor="red";
+}
+
+/*let x=12
+let y="Hello"
+let z=true
+let obj={
+    name:"John",
+    age:25,
+    isEmployed:true
+}
+let arr=[1,2,3,4,5]
+function display(){
+    //console.log("Hello World",typeof x,typeof y,typeof z,typeof obj,typeof arr);
+}
+function validate(){
+    let name=document.getElementById("name").value;
+    let password=document.getElementById("pass").value;
+    if(name==="admin" && password==="admin"){
+        document.getElementById("result").innerHTML="Login Success";
+    }
+    else{
+        //document.getElementById("result").innerHTML="Login Failed";
+    }
+}*/
+/*let x=Boolean(0);
+console.log(x);
+a=12
+b="12"
+console.log(a==b);
+console.log(a===b);*/
+/*let a=(x,y)=>{
+    return(x+y);
+}
+console.log(a(12,34))
+let b=()=>{
+    console.log("Hello World");
+}
+b();*/
